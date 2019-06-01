ca83b85153cbca31626fdfdd04c0632b683e45dc
Fabio de Stefani <fabiostefani@gmail.com>
2019-06-01 06:45:03 -0300

    EntityFramework

---
+++ Data/StoreDataContext.cs
@@ -0,0 +1,20 @@
+﻿using fabiostefani.io.MapaCatalog.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fabiostefani.io.MapaCatalog.Api.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Host = localhost; Port = 5432; Pooling = true; Database = MapaCatalog; User Id = postgres; Password = Postgres2018!; ");
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace fabiostefani.io.MapaCatalog.Api.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
